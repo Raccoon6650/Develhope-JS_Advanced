4b0d94052a53d79b1aac5f6383e81476e156d451
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-10 18:50:56 +0100

    Esercizio completato

---
+++ es-39/exercise.js
@@ -0,0 +1,16 @@
+function multiplyByTwo(value) {
+  let number = 2;
+
+   function inner (){
+    console.log( value*number);
+   }
+   inner();
+}
+
+multiplyByTwo(2);
+multiplyByTwo(4);
+multiplyByTwo(8);
+
+
+
+
