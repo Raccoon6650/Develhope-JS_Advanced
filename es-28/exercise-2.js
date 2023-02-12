9b1816ddeeb09e5a627951e8472b9f452ccee6d2
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-12 23:21:13 +0100

    Esercizio teoricamente corretto

--- es-28/exercise-2.js
+++ es-28/exercise-2.js
@@ -16,4 +16,3 @@ if (!order.customer?.address.city) {
 //In ogni caso ho messo la verifica della presenza di ".address" e come commento, la negazione dello stesso.
 //Nel primo caso il risultato è analogo a quanto presente nella base in quanto order risulta vuoto e l'if non valido (togliendo la verifica dell' "?." risulta in errore)
 //Nel secondo caso l'if risulta vuoto perché si nega il fatto che sia vuoto e la presenza o meno di "?." non sortisce effetti.
-//Se fosse possibile vorrei una spiegazione del corretto utilizzo di "?." perché ne ho capito lo scopo ma non mi è chiarissimo come usarlo. Grazie in anticipo Nino
