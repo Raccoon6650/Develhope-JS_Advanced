158815c3eef8641942d538da3b808fc5bbc1b4a5
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-12 22:15:26 +0100

    Non ho capito l'esercizioooo

---
+++ es-23/exercise-1.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+for (let key of Object.keys(person)) {
+  i=0; i+1;
+  console.log(key, ": ",person[i].value); 
+}
+
+// Print values of person using Object.keys
+ 
