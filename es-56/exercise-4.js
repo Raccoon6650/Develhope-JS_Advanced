1ec21a7f4aea6eac9b497288b808f8cb96a52c55
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-16 11:50:47 +0100

    Test non riusciti

---
+++ es-56/exercise-4.js
@@ -0,0 +1,61 @@
+function uncompletedNotes(array) {
+  /*let newArray = [];
+  array.forEach(() => newArray.push(Object.values(array.id)))
+
+  console.log(newArray);*/
+ array.forEach(element => {
+  (console.log(Object.values(array)));
+  });
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+uncompletedNotes(notes);
