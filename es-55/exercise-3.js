cf355480ca5ad519b65ebb6a84430f99b367462b
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-16 09:45:32 +0100

    Esercizio terminato

---
+++ es-55/exercise-3.js
@@ -0,0 +1,29 @@
+function calculateAverageAge(array) {
+  let newArray = [];
+  for(i=0; i<array.length; i++){
+    newArray.push(array[i].age)
+  }
+
+  let sum = newArray.reduce( function(total, other){
+    return total + other
+  });
+  let average = Math.round(sum/newArray.length);
+  return average
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
